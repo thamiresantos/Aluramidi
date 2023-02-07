c01d476e55bc3ef725836ca8c3d30dc9364586f8
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-02-06 22:54:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom (idElementAudio){
-    document.querySelector(idElementAudio).play();
+function tocaSom (seletorAudio){
+  const elemento = document.querySelector(seletorAudio)
+
+
+  if (elemento != null && elemento.localName === 'audio') {
+     elemento.play();
+  } 
+  else {
+    //alert('Elemento não encontrado');
+    console.log('Elemento não encontrado ou seletor inválido ');
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -17,4 +26,20 @@ for ( let contador = 0; contador < listaDeTeclas.length; contador ++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function(evento) {
+
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space' || evento.code === 'Enter' ){
+           tecla.classList.add('ativa'); 
+        }
+
+
+        
+    }
+
+    tecla.onkeyup = function() {
+
+            tecla.classList.remove('ativa')
+    }
 }
